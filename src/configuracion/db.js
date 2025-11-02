5bfa081dc1db705ee58670ad799fdc582261795f
lucabotteri169@outlook.com <lucabotteri169@outlook.com>
2025-11-01 23:26:10 -0600

    Agrega controlador y rutas de imágenesUsuarios + ajustes en relaciones

--- src/configuracion/db.js
+++ src/configuracion/db.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-const USUARIO = process.env.USUARIO || "";
-const CONTRASENA = process.env.CONTRASENA || "";
-const DB = process.env.DB || "";
+const USUARIO = process.env.USUARIO || "root";
+const CONTRASENA = process.env.CONTRASENA || "12345";
+const DB = process.env.DB || "proyectoseminario";
 
 const database = new Sequelize(DB, USUARIO, CONTRASENA, {
   dialect: "mysql",
