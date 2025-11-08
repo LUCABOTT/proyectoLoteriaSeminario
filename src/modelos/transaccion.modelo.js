708f285d5ab6191dfb22016f3dae78f32d3c8476
Pablo Sosa <sosadavid4280@gmail.com>
2025-11-08 17:56:12 -0600

    Fix: alineado tipo FK billetera INTEGER

--- src/modelos/transaccion.modelo.js
+++ src/modelos/transaccion.modelo.js
@@ -5,7 +5,7 @@ const Transaccion = database.define(
   "Transaccion",
   {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    billetera: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false },
+    billetera: { type: DataTypes.INTEGER, allowNull: false },
     monto: { type: DataTypes.DECIMAL, allowNull: false },
     tipo: {
       type: DataTypes.ENUM("Recarga", "Pago", "Reembolso"),
